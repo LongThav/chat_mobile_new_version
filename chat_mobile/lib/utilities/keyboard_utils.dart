a6eb9a0ce2d592b2def8a0285a82ab195e28be84
SiPav <longthavsipav69@gmail.com>
2023-06-25 11:15:44 +0700

    Updatr

--- chat_mobile/lib/utilities/keyboard_utils.dart
+++ chat_mobile/lib/utilities/keyboard_utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 class KeyboardUtils {
   static bool isKeyboardShowing() {
+    // ignore: unnecessary_null_comparison
     if (WidgetsBinding.instance != null) {
       return WidgetsBinding.instance.window.viewInsets.bottom > 0;
     } else {
